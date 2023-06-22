b2f97f2ba2ba46fd7ac272a4441f7876e4dc650a
mustafagermec <mustafa.germec@powercoders.org>
2023-06-22 20:35:30 +0200

    add say hello code

--- 01_week/bioinformatic.py
+++ 01_week/bioinformatic.py
@@ -1,7 +1,11 @@
 print('Hello World!')
 lis = [2, 5, 7, 8, 12]
 for i in lis:
-    if i%2 == 0:
-        print('the bnumber is even')
+    if i % 2 == 0:
+        print('the number is even')
     else:
         print('the number is odd')
+
+
+def say_hello():
+    print('hello!')
